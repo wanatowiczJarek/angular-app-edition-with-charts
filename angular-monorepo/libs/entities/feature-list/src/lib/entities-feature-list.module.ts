a1bbb2eefeb1d675fdc1f388d6c255a588e68327
amanitaGhost <amanitaGhost@proton.me>
2023-12-17 15:02:36 +0100

    List component added

--- angular-monorepo/libs/entities/feature-list/src/lib/entities-feature-list.module.ts
+++ angular-monorepo/libs/entities/feature-list/src/lib/entities-feature-list.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: ListComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
 })
-export class EntitiesFeatureListModule {}
+export class EntitiesFeatureListModule { }
---
+++ angular-monorepo/libs/entities/feature-list/src/lib/list/list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-monorepo-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent { }
