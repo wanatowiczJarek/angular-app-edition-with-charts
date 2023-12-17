5365380b61c65c80d1254b59e22852cfa32bb850
amanitaGhost <amanitaGhost@proton.me>
2023-12-17 15:06:14 +0100

    Location dashboard component added

--- angular-monorepo/libs/entities/feature-location-dashboard/src/lib/entities-feature-location-dashboard.module.ts
+++ angular-monorepo/libs/entities/feature-location-dashboard/src/lib/entities-feature-location-dashboard.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { LocationDashboardComponent } from './location-dashboard/location-dashboard.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: LocationDashboardComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
 })
-export class EntitiesFeatureLocationDashboardModule {}
+export class EntitiesFeatureLocationDashboardModule { }
---
+++ angular-monorepo/libs/entities/feature-location-dashboard/src/lib/location-dashboard/location-dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-monorepo-location-dashboard',
+  templateUrl: './location-dashboard.component.html',
+  styleUrls: ['./location-dashboard.component.scss'],
+})
+export class LocationDashboardComponent { }
